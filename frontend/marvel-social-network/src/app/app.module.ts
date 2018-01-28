700c28582d406e4633bba4f901e4d6f36dc93ce0
Nikita Marinosyan <nikita.marinosyan@gmail.com>
2018-01-28 16:02:00 +0300

    Started implementing firebase auth

--- frontend/marvel-social-network/src/app/app.module.ts
+++ frontend/marvel-social-network/src/app/app.module.ts
@@ -1,17 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: 'AIzaSyD0gFkxcO2tzKxD8Ao6fXtuhgeglD7z3f4',
+  authDomain: 'marvel-3d-network.firebaseapp.com',
+  databaseURL: 'https://marvel-3d-network.firebaseio.com',
+  projectId: 'marvel-3d-network',
+  storageBucket: 'marvel-3d-network.appspot.com',
+  messagingSenderId: '451920496171'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
