485fc697a60d79beb9e4848f4456f4e8559a0e1b
Nikita Marinosyan <nikita.marinosyan@gmail.com>
2018-01-28 22:06:27 +0300

    Created login page

--- frontend/marvel-social-network/src/app/app.module.ts
+++ frontend/marvel-social-network/src/app/app.module.ts
@@ -1,30 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { MatCardModule, MatButtonModule, MatToolbarModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { AngularFireModule } from 'angularfire2';
+
+import { RouterModule } from '@angular/router';
+import { routes } from './routes';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SocialNetworkComponent } from './social-network/social-network.component';
 import { AboutComponent } from './about/about.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 
-import { RouterModule } from '@angular/router';
-import { routes } from './routes';
+const firebaseConfig = {
+  apiKey: 'AIzaSyD0gFkxcO2tzKxD8Ao6fXtuhgeglD7z3f4',
+  authDomain: 'marvel-3d-network.firebaseapp.com',
+  databaseURL: 'https://marvel-3d-network.firebaseio.com',
+  projectId: 'marvel-3d-network',
+  storageBucket: 'marvel-3d-network.appspot.com',
+  messagingSenderId: '451920496171'
+};
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SocialNetworkComponent,
-    AboutComponent,
-    FooterComponent,
-    HeaderComponent
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    RouterModule.forRoot(routes)
+    FormsModule,
+    RouterModule.forRoot(routes),
+    MatCardModule, MatButtonModule, MatToolbarModule,
+    FlexLayoutModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/marvel-social-network/src/app/login/login.component.ts
+++ frontend/marvel-social-network/src/app/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  error: any;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  loginFb() {}
+
+  loginGoogle() {}
 
 }
