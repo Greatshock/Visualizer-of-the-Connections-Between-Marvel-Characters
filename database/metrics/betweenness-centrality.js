752cdd6132d2a24ec478a93a49469d7fecf4edcb
Nikita Marinosyan <nikita.marinosyan@gmail.com>
2018-03-27 22:49:24 +0300

    Paths finding algorithm implemeted

--- database/metrics/betweenness-centrality.js
+++ database/metrics/betweenness-centrality.js
@@ -3,21 +3,51 @@ const fs = require('fs');
 const nodes = JSON.parse(fs.readFileSync('../characters_for_nodes.json', 'utf8'));
 const links = JSON.parse(fs.readFileSync('../links_between_characters.json', 'utf8'));
 
-// Function to calculate shortest paths between 2 nodes
-function getShortestPaths(node1, node2) {
-    const paths = getPathsBetweenNodes(node1, node2);
-    console.log(paths);
-}
-getShortestPaths(nodes[0], nodes[1]);
+const newNodes = getNodesWithNeighbors();
+let paths = [];
 
-function getPathsBetweenNodes(node1, node2) {
-    return []
+function visitNode(a, b, path) {
+    //console.log('current node is ' + a.id);
+    if (a.id === b.id) {
+        // Target reached
+        paths.push(path);
+        return;
+    }
+
+    // Mark the node
+    a.visited = true;
+
+    // Try all unmarked neighbors
+    let aNeighbors = a.neighbors;
+    //console.log('number of neighbors = ' + a.neighbors.length);
+    for (let i = 0; i < aNeighbors.length; i++) {
+        if (!newNodes[aNeighbors[i]].visited) {
+            visitNode(newNodes[aNeighbors[i]], b, path + ' -> ' + newNodes[aNeighbors[i]].id);
+        }
+    }
+
+    //a.visited = false;
 }
 
-// function getAllShortestPaths(nodes) {
-//     for (var i = 0; i < nodes.length - 1; ++i) {
-//         for (var j = i; j < nodes.length; ++i) {
-//             getShortestPaths(nodes[i], nodes[j])
-//         }
-//     }
-// }
+function getNodesWithNeighbors() {
+    let nodesWithNeighbors = {};
+    for (let i = 0; i < nodes.length; i++) {
+        let node = {};
+        node.id = nodes[i].id;
+        node.visited = false;
+        node.neighbors = [];
+        nodesWithNeighbors[nodes[i].id] = node;
+    }
+
+    for (let i = 0; i < links.length; i++) {
+        nodesWithNeighbors[links[i].source].neighbors.push(links[i].target);
+        nodesWithNeighbors[links[i].target].neighbors.push(links[i].source);
+    }
+
+    return nodesWithNeighbors;
+}
+
+visitNode(newNodes['1009220'], newNodes['1009215'], '1009220');
+
+console.log('THE PATH IS: ' + paths[0]);
+console.log(paths.length);
