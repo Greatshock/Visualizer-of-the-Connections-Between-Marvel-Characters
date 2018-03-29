a1a398f4b1b73f3482470900ea63755e5b2dacae
Nikita Marinosyan <nikita.marinosyan@gmail.com>
2018-03-29 22:34:00 +0300

    Added stats on basic characteristics
    - Pie charts added
    - Tables added

--- marvel-social-network/src/app/stats/stats.component.ts
+++ marvel-social-network/src/app/stats/stats.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {MatTableDataSource} from "@angular/material";
-import {genderDistribution, GenderRow} from "./data/data";
+import { MatTableDataSource } from "@angular/material";
+import { genderDistribution, raceDistribution, citizenshipDistribution, orientationDistribution } from "./data/data";
 
 @Component({
   selector: 'app-stats',
@@ -9,16 +9,64 @@ import {genderDistribution, GenderRow} from "./data/data";
 })
 export class StatsComponent {
 
-  constructor() { }
+  gendersChartLabels:string[] = [];
+  gendersChartData:number[] = [];
+  racesChartLabels:string[] = [];
+  racesChartData:number[] = [];
+  citizenshipsChartLabels:string[] = [];
+  citizenshipsChartData:number[] = [];
+  orientationsChartLabels:string[] = [];
+  orientationsChartData:number[] = [];
 
-  displayedColumns = ['gender', 'amount', 'percentage'];
-  dataSource = new MatTableDataSource(GENDERS_DATA);
+  constructor() {
+    for (let i = 0; i < genderDistribution.length; i++) {
+      this.gendersChartData.push(genderDistribution[i].amount);
+      this.gendersChartLabels.push(genderDistribution[i].gender);
+    }
+    for (let i = 0; i < raceDistribution.length; i++) {
+      this.racesChartData.push(raceDistribution[i].amount);
+      this.racesChartLabels.push(raceDistribution[i].race);
+    }
+    for (let i = 0; i < citizenshipDistribution.length; i++) {
+      this.citizenshipsChartData.push(citizenshipDistribution[i].amount);
+      this.citizenshipsChartLabels.push(citizenshipDistribution[i].citizenship);
+    }
+    for (let i = 0; i < orientationDistribution.length; i++) {
+      this.orientationsChartData.push(orientationDistribution[i].amount);
+      this.orientationsChartLabels.push(orientationDistribution[i].orientation);
+    }
+  }
 
-  applyFilter(filterValue: string) {
+  displayedGenderColumns = ['gender', 'amount', 'percentage'];
+  gendersDataSource = new MatTableDataSource(genderDistribution);
+  applyGenderFilter(filterValue: string) {
     filterValue = filterValue.trim(); // Remove whitespace
     filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
-    this.dataSource.filter = filterValue;
+    this.gendersDataSource.filter = filterValue;
   }
-}
 
-const GENDERS_DATA: GenderRow[] = genderDistribution;
+  displayedRaceColumns = ['race', 'amount', 'percentage'];
+  racesDataSource = new MatTableDataSource(raceDistribution);
+  applyRaceFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.racesDataSource.filter = filterValue;
+  }
+
+  displayedCitizenshipColumns = ['citizenship', 'amount', 'percentage'];
+  citizenshipsDataSource = new MatTableDataSource(citizenshipDistribution);
+  applyCitizenshipFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.citizenshipsDataSource.filter = filterValue;
+  }
+
+  displayedOrientationColumns = ['orientation', 'amount', 'percentage'];
+  orientationsDataSource = new MatTableDataSource(orientationDistribution);
+  applyOrientationFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    filterValue = filterValue.toLowerCase(); // MatTableDataSource defaults to lowercase matches
+    this.orientationsDataSource.filter = filterValue;
+  }
+
+}
